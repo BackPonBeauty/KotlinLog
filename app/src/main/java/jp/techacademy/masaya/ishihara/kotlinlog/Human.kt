520c79c646af02ca216c3bd44e07e99294d63dc9
BackPonBeauty <vf2_casino@yahoo.co.jp>
2022-06-22 23:09:04 +0900

    huh?

--- app/src/main/java/jp/techacademy/masaya/ishihara/kotlinlog/Human.kt
+++ app/src/main/java/jp/techacademy/masaya/ishihara/kotlinlog/Human.kt
@@ -1,18 +1,16 @@
 package jp.techacademy.masaya.ishihara.kotlinlog
 import android.util.Log
-open class Human: Animal,Thinkable {
+open class Human: Animal,Thinkable{
+    // プロパティ
 
-        // superで親クラスのコンストラクタを呼ぶ
-        constructor(name: String, age: Int, hobby: String): super(name, age, hobby) {
-        }
-
-
-        override fun say() {
-        Log.d("kotlintest", "私の名前は" + this.name + "です。年は" + this.age + "歳" + "です。")
+   var hobby: String
+    constructor(name: String, age: Int,hobby: String) : super(name,age) {
+        this.hobby = hobby
     }
-
-        // Thinkableインターフェイスのメソッドをオーバーライド
-        override fun think() {
-                Log.d("kotlintest", "私は" + this.hobby + "について考える")
-        }
+    // 引数付きコンストラクタ
+  //  constructor(name: String, age: Int) : super(name,age) {}
+    override fun say() {
+    }
+   override fun think() {
+    }
 }
